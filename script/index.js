32e67de5dc8babe0d9017ed58671fe309915a43c
bBSempai <bbwhacker0805@gmail.com>
2021-04-17 01:54:07 +0530

    Improved Projects' display

--- script/index.js
+++ script/index.js
@@ -1,19 +1,21 @@
 const translate = document.querySelectorAll(".translate");
 
 window.addEventListener("scroll", () => {
-  let scroll = window.pageYOffset;
-  let offsetScrollTwiceBuy = scroll - 1915;
-  let offsetScrollCinemaCrate = scroll - 2745;
-  // console.log(scroll , "----", offsetScrollTwiceBuy);
+    let scroll = window.pageYOffset;
+    let offsetScrollTwiceBuy = scroll - 1915;
+    let offsetScrollCinemaCrate = scroll - 2745;
+    // console.log(scroll , "----", offsetScrollTwiceBuy);
 
-  translate.forEach((element) => {
-    let speed = element.dataset.speed;
-    element.style.transform = `translateY(${scroll * speed}px)`;
-  });
+    // translate.forEach((element) => {
+    //   let speed = element.dataset.speed;
+    //   element.style.transform = `translateY(${scroll * speed}px)`;
+    // });
 
-  let twicebuy = document.getElementById("twicebuy-image");
-  let cinemacrate = document.getElementById("cinemacrate-image");
+    let maining = document.getElementById("mainImg");
+    let twicebuy = document.getElementById("TwiceBuy-image");
+    let cinemacrate = document.getElementById("CinemaCrate-image");
 
-  twicebuy.style.top = offsetScrollTwiceBuy * 0.9 + "px";
-  cinemacrate.style.top = offsetScrollCinemaCrate * 0.9 + "px";
+    maining.style.top = scroll * 0.8 + "px";
+    twicebuy.style.top = offsetScrollTwiceBuy * 0.9 + "px";
+    cinemacrate.style.top = offsetScrollCinemaCrate * 0.9 + "px";
 });
